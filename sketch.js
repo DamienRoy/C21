dbc66e136b6cb38e9e28df81aac30b849dbf6022
DamienRoy <88617030+DamienRoy@users.noreply.github.com>
2021-11-11 17:06:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+var ball;
+var button;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  button = createImg("push.png")
+  button.position(200,10)
+  button.size(50,50)
+  button.mouseClicked(force)
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+ var options = {
+   restitution:0.9
+ }
+ball = Bodies.circle(200,100,20,options)
+World.add(world,ball)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  ellipse(ball.position.x,ball.position.y,20)
+  Engine.update(engine);
+
+}
+
+function force(){
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05})
+}
+
+
+
+
